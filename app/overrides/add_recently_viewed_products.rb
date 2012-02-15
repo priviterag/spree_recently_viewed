c85ed957e26867cb54273559e948f432a6a00abf
Giuseppe Privitera <priviterag@gmail.com>
2012-02-15 16:03:46 +0100

    updated original deface logic

--- app/overrides/add_recently_viewed_products.rb
+++ app/overrides/add_recently_viewed_products.rb
@@ -1,9 +1,32 @@
-Deface::Override.new(:virtual_path => 'spree/shared/_products',
-                     :name => 'add_recently_viewed_products_to_products_index',
-                     :insert_after => "#products[data-hook], [data-hook='products']",
-                     :partial => 'spree/shared/recently_viewed_products')
+# Deface::Override.new(:virtual_path => 'spree/shared/_products',
+#                      :name => 'add_recently_viewed_products_to_products_index',
+#                      :insert_after => "#products[data-hook], [data-hook='products']",
+#                      :partial => 'spree/shared/recently_viewed_products')
 
-Deface::Override.new(:virtual_path => 'spree/products/show',
-                     :name => 'add_recently_viewed_products_to_products_show',
-                     :insert_after => "#product_description[data-hook], [data-hook='product_description']",
-                     :partial => 'spree/shared/recently_viewed_products')
+# Deface::Override.new(:virtual_path => 'spree/products/show',
+#                      :name => 'add_recently_viewed_products_to_products_show',
+#                      :insert_after => "#product_description[data-hook], [data-hook='product_description']",
+#                      :partial => 'spree/shared/recently_viewed_products')
+
+
+Deface::Override.new(
+  :virtual_path => "spree/home/index",
+  :insert_after => "[data-hook='homepage_sidebar_navigation']",
+  :partial      => "spree/shared/recently_viewed_products",
+  :name         => "add_recently_viewed"
+)
+
+Deface::Override.new(
+  :virtual_path => "spree/products/index",
+  :insert_after => "[data-hook='homepage_sidebar_navigation']",
+  :partial      => "spree/shared/recently_viewed_products",
+  :name         => "add_recently_viewed"
+)
+
+Deface::Override.new(
+  :virtual_path => "spree/taxons/show",
+  :insert_after => "[data-hook='taxon_sidebar_navigation']",
+  :partial      => "spree/shared/recently_viewed_products",
+  :name         => "add_recently_viewed"
+)
+
